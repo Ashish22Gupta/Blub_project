619b8ac793878e41e083a9028e374235b1122676
Gupta Ashish Maharanidin <154006215+Ashish22Gupta@users.noreply.github.com>
2024-03-12 09:22:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function myfun1(){
+            document.getElementById('b1').style.display = "block";
+            document.getElementById('b2').style.display = "none";
+        }
+        function myfun2(){
+            document.getElementById('b1').style.display = "none";
+            document.getElementById('b2').style.display = "block";
+        }
