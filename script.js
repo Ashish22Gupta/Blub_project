a95a6abd8db77e893a3c35114bb2bed1756e2f9b
Gupta Ashish Maharanidin <154006215+Ashish22Gupta@users.noreply.github.com>
2025-01-11 09:50:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function myfun1(){
-            document.getElementById('b1').style.display = "block";
-            document.getElementById('b2').style.display = "none";
-        }
-        function myfun2(){
-            document.getElementById('b1').style.display = "none";
-            document.getElementById('b2').style.display = "block";
-        }
+function toggleBulbs() {
+    const isChecked = document.getElementById('toggleSwitch').checked;
+    if (isChecked) {
+        document.getElementById('b1').style.display = "none";
+        document.getElementById('b2').style.display = "block";
+    } else {
+        document.getElementById('b1').style.display = "block";
+        document.getElementById('b2').style.display = "none";
+    }
+}
